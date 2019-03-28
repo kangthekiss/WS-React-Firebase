9ef73336a1fffd370bb4e44a996c0b806cb2df14
KangTheKiss <kangthekiss@gmail.com>
2019-03-28 21:55:34 +0700

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
 import React, { Component } from 'react'
-import Header from './components/Header'
-import MessageList from './components/MessageList'
-import firebase from 'firebase'
-import MessageBox from './components/MessageBox'
+import { Link } from 'react-router-dom'
+import firebase from './Firebase'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    var config = {
-      apiKey: "AIzaSyAq_CCh7kOMfG0TXgJ1T9SmLq_s9bF-I7Q",
-      authDomain: "react-firebase-356ab.firebaseapp.com",
-      databaseURL: "https://react-firebase-356ab.firebaseio.com",
-      projectId: "react-firebase-356ab",
-      storageBucket: "react-firebase-356ab.appspot.com",
-      messagingSenderId: "733493025234"
-    };
-    firebase.initializeApp(config)
+    this.ref = firebase().firestore().collection(boards)
+    this.unsubscribe = null
+    this.state = {
+      boards: []
+    }
   }
-  
 
   render() {
     return (
-      <div className="container">
-
-        <Header title="React Firebase Tutorial" />
-
-        <div className="columns">
-          <div className="column is-3">
-
-          </div>
-          <div className="column is-6">
-            <MessageList db={firebase} />
-          </div>
-        </div>  
-
-        <div className="columns">
-          <div className="column is-3">
-
-          </div>
-          <div className="column is-6">
-            <MessageBox db={firebase} />
-          </div>
-        </div>  
+      <div className="container"> 
            
       </div>
     )
---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+const settings = { timestampsInSnapshots: true}
+
+const config = {
+    apiKey: "AIzaSyAq_CCh7kOMfG0TXgJ1T9SmLq_s9bF-I7Q",
+    authDomain: "react-firebase-356ab.firebaseapp.com",
+    databaseURL: "https://react-firebase-356ab.firebaseio.com",
+    projectId: "react-firebase-356ab",
+    storageBucket: "react-firebase-356ab.appspot.com",
+    messagingSenderId: "733493025234"
+}
+
+firebase.initializeApp(config)
+
+firebase.firestore().settings(settings)
+
+export default firebase
